467735ca5f715fa5005d05469c9c4c6b5f847541
Mvcampbell3 <mvcampbell3@gmail.com>
2018-12-05 11:46:36 -0500

    added most of the material, time to style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 // Constructor for new items in game
-function Item(name, noSpaces, audio, image) {
+function Item(name, noSpaces, audio, image, show) {
     this.name = name;
     this.letters = noSpaces.split("");
     this.audioID = audio;
     this.imageID = image;
-
 };
 // Collection of the var new Items
 // Make sure spelling is same for both, Will not go to space today if wrong
 var fezzik = new Item("fezzik", "fezzik", "audiotag1", "assets/images/fezzik.jpg");
-var vizzini = new Item("vizzini", "vizzini", "audiotag2", "assets/images/fezzik.jpg");
-var max = new Item("miracle max", "miraclemax", "audiotag3", "assets/images/fezzik.jpg");
-
+var vizzini = new Item("vizzini", "vizzini", "audiotag2", "assets/images/Vizzini.png");
+var max = new Item("miracle max", "miraclemax", "audiotag3", "assets/images/miracle.jpg");
+var count = new Item("count rugen", "countrugen", "audiotag4", "assets/images/count.jpg");
+var inigo = new Item("inigo montoya", "inigomontoya", "audiotag5", "assets/images/inigo.png");
+var humperdink = new Item("prince humperdink", "princehumperdink", "audiotag6", "assets/images/humperdink.jpg");
+var buttercup = new Item("princess buttercup", "princessbuttercup", "audiotag7", "assets/images/buttercup.png");
+var westley = new Item("dread pirate roberts", "dreadpirateroberts", "audiotag8", "assets/images/captainRoberts.jpg");
 
 // Array that holds the new Items
-var gameArray = [fezzik, vizzini, max]
+var gameArray = [fezzik, vizzini, max, count, inigo, humperdink, buttercup, westley];
+// var gameArray = [fezzik];
 
 
 
@@ -28,9 +32,11 @@ var game = {
     active: "",
     correct: [],
     interactive: [],
+    image: "",
     chances: 6,
     wins: 0,
     losses: 0,
+    opacityBlack: 1,
     // output ids for HTML
     winsOut: document.getElementById("wins"),
     lossesOut: document.getElementById("losses"),
@@ -38,6 +44,7 @@ var game = {
     chancePlace: document.getElementById("chancePlace"),
     usedPlace: document.getElementById("usedPlace"),
     nextRound: document.getElementById("nextRound"),
+    picPlace: document.getElementById("gamePic"),
     // methods used to run the game
 
     start: function () {
@@ -45,10 +52,12 @@ var game = {
             // Pick an Array
             var number = Math.floor((Math.random() * gameArray.length) + 0);
             game.active = gameArray[number];
+            game.whichArray = number;
             // Set vars in game to item properties
             game.correct = gameArray[number].letters;
             game.audio = gameArray[number].audioID;
             game.interactive = gameArray[number].name.split("");
+            game.image = gameArray[number].imageID;
             gameArray.splice(number, 1);
             console.log(game.correct);
             console.log(number + " index of array in gameArray set");
@@ -70,8 +79,11 @@ var game = {
         } else {
             // Game Over Stuff
             console.log("out of arrays")
-            game.output.innerHTML = "<h1>Game is over!!</h1>";
+            game.output.innerHTML = "<h1 class='huge'>Game Is Over!!</h1>";
             document.getElementById("hideUsed").style.display = "none";
+            document.getElementById("hideChance").style.display = "none";
+            document.getElementById("container").style.display = "block";
+            document.getElementById("gamePic").style.display = "none";
         }
     },
 
@@ -94,7 +106,7 @@ var game = {
             game.newRound();
         } else if (game.guess == "Enter" && game.correct.length > 0) {
             console.log("enter pressed and have not finished word");
-
+            game.newRound();
         } else {
 
             // This is magic the likes of which I have not seen nor can I attempt to explain
@@ -115,13 +127,13 @@ var game = {
                 game.tripped = true;
 
                 this.checkGuess();
-            
-            // No penatly for using letter more than once
+
+                // No penatly for using letter more than once
             } else if (game.used.indexOf(game.guess) != -1) {
                 console.log("was used before");
 
-            // Eventually letter will get through to here, if was correct at one point, tripped will be true
-            // Letter added to used array and displayed
+                // Eventually letter will get through to here, if was correct at one point, tripped will be true
+                // Letter added to used array and displayed
             } else {
                 console.log("not in array");
                 game.used.push(game.guess);
@@ -137,7 +149,7 @@ var game = {
             }
         }
     },
-    
+
     checkWord: function () {
         // Since all letters will eventually get here, need to check if word is complete
         if (game.chances > 0) {
@@ -147,16 +159,18 @@ var game = {
                 document.getElementById(game.audio).play();
                 game.wins++;
                 game.winsOut.textContent = game.wins;
+                game.picPlace.style.backgroundImage = "url("+game.image+")";
                 game.beforeNext();
             } else {
                 console.log("keep guessing");
                 game.playerPress();
             };
         } else {
-            // GAME OVER STUFF
+            // Round over stuff
             game.losses++
+            document.getElementById("audiotagLost").play();
             game.lossesOut.textContent = game.losses;
-            game.newRound();
+            game.beforeNext();
         }
     },
     // Resets all of the properties to empty, not really neccesary because game.start would override but I like it.
@@ -167,16 +181,18 @@ var game = {
         game.chancePlace.textContent = game.chances;
         game.used = [];
         game.usedPlace.textContent = game.used;
+        document.getElementById("hideUsed").style.display = "block";
         game.nextRound.innerHTML = "";
+        game.picPlace.style.backgroundImage = "";
         document.getElementById(game.audio).pause();
         game.audio = "";
         game.start();
     },
     // Pause before newRound is run if you are right. Displays the word and waits for enter key to be pressed through game.checkGuess
     beforeNext: function () {
-        game.output.innerHTML = "<h3 class='between'>The Answer Was " + game.active.name.toUpperCase() + "</h3>";
-        game.nextRound.innerHTML = "<h3>Press Enter to continue</h3>"
-
+        game.output.innerHTML = "<h3 id='between'>" + game.active.name.toUpperCase() + "</h3>";
+        game.nextRound.innerHTML = "<h2 id='press'>Press Enter to continue</h2>";
+        document.getElementById("hideUsed").style.display = "none";
     }
 
 }
@@ -184,6 +200,10 @@ var game = {
 
 
 var modal = {
+    sum: function(){
+        document.getElementById("themeAudio").play();
+        console.log("playing start audio");
+    },
     id: document.getElementById("modal"),
     rules: document.getElementById("rules"),
     main: document.getElementById("main-body"),
@@ -204,3 +224,6 @@ var modal = {
         }, 25)
     }
 }
+
+
+
